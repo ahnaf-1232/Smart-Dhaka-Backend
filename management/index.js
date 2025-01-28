e57a956c1e09cb3712935f476cd40f73d6e7c255
ahnaf-1466-bs <ahnaf.mubashshir@brainstation-23.com>
2025-01-28 19:31:18 +0600

    update 3

--- management/index.js
+++ management/index.js
@@ -9,8 +9,6 @@ app.use(bodyParser.json());
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     ssl: true,
     tlsCAFile: "./global-bundle.pem",
   })
